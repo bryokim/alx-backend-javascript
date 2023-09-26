ac0845d2a95ff85630c1927a17921cd4dc270037
Brian Kimathi <bryo.kim1@gmail.com>
2023-09-26 19:56:49 +0300

    Formatted to eslint specification

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,9 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((data) => {
-    return data;
-  });
+  ]).then((data) => data);
 }
