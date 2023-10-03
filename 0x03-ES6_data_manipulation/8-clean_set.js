178fd2f55f7e704e01297515de4a6f6466e0f0d9
Brian Kimathi <bryo.kim1@gmail.com>
2023-10-03 21:16:45 +0300

    Trim spaces before using the startString

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,13 @@
 export default function cleanSet(set, startString) {
   const array = [];
+  const str = startString.trim();
 
-  if (startString === '') {
+  if (str === '') {
     return '';
   }
 
   set.forEach((value) => {
-    if (value.startsWith(startString)) array.push(value.substring(startString.length));
+    if (value.startsWith(str)) array.push(value.substring(str.length));
   });
 
   return array.join('-');
